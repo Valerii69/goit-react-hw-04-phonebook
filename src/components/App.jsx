45f90fc704c90b5151b7a27a4254c0b3580e8680
Valerii Viun <2455521@gmail.com>
2023-06-16 18:25:10 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,32 +18,6 @@ export function App() {
   ]);
 
   const [filter, setFilter] = useState('');
-  //   contacts: [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ],
-  //   filter: '',
-  // };
-  //монтування Component
-  // componentDidMount() {
-  //   const dataComponent = JSON.parse(localStorage.getItem('contacts'));
-  //   console.log(dataComponent);
-
-  //   if (dataComponent) {
-  //     this.setState({ contacts: dataComponent });
-  //   }
-  // }
-  //оновлення Component
-  // componentDidUpdate(prevProps, prevState) {
-  //   // console.log('App componentDidUpdate');
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     // console.log('Обновилось поле contacts ');
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
-  // створення нового контакта
 
   const addNewContact = data => {
     if (
